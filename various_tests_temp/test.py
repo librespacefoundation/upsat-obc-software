07ad1b5e0f128c7f067703a2fe6a79f81a059a6c
nchronas <nchronas@gmail.com>
2016-03-27 20:22:06 +0300

    added tests for verification

--- various_tests_temp/test.py
+++ various_tests_temp/test.py
@@ -10,12 +10,18 @@ t = array.array('B', [126,24,1,192,185,0,5,16,17,1,6,0,99,126]).tostring() #test
 
 t = array.array('B', [126,24,1,192,185,0,10,16,8,1,6,1,0,0,0,8,0,124,126]).tostring() #led on
 
-t = array.array('B', [126,24,1,192,185,0,10,16,8,1,6,0,0,0,0,8,0,125,126]).tostring() #led off
+t = array.array('B', [126,24,1,192,185,0,10,16,8,1,6,0,0,0,0,8,0,125,93,126]).tostring() #led off
+
+t = array.array('B', [126,24,1,192,185,0,10,17,8,1,6,1,0,0,0,8,0,125,93,126]).tostring() #led on with ack
+
+#t = array.array('B', [126,24,1,192,185,0,10,17,8,1,6,0,0,0,0,8,0,124,126]).tostring() #led off with ack
 port.write(t)
 
 time.sleep(1)
 
 while True:
     rcv = port.read(10)
-    print rcv.encode('hex')
+    print rcv, rcv.encode('hex')
     time.sleep(1)
+
+
