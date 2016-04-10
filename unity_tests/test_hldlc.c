8e5143373a15e10d18048e77cf2dc8c20470f58d
Alfredos-Panagiotis Damkalis <fredy@fredy.gr>
2016-04-10 15:04:09 +0300

    Adding error checks on hldlc.c unit test and changing its format

--- unity_tests/test_hldlc.c
+++ unity_tests/test_hldlc.c
@@ -1,12 +1,17 @@
 #include "unity.h"
 #include "hldlc.h"
 #include "Mockservices.h"
+#include <stdio.h>
 
 #define TEST_ARRAY 20
 
+/*
+* =========
+* | frame |
+* =========
+*/
 
-
-void test_hldlc_frame1(void) {
+void test_hldlc_frame_without_escape_octet(void) {
     uint8_t check;
     uint16_t size = 10;
     uint16_t res_size = 12;
@@ -17,12 +22,12 @@ void test_hldlc_frame1(void) {
 
     check = HLDLC_frame(in, out, &size);
 
-    TEST_ASSERT_TRUE(check == SATR_EOT);
+    TEST_ASSERT_TRUE_MESSAGE(check == SATR_EOT, "Function didn't return SATR_EOT");
 
-    TEST_ASSERT_EQUAL_UINT8_ARRAY(res, out, res_size);
+    TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE(res, out, res_size, "Output wasn't the expected one");
 }
 
-void test_hldlc_frame2(void) {
+void test_hldlc_frame_with_escape_octet(void) {
     uint8_t check;
     uint16_t size = 10;
     uint16_t res_size = 14;
@@ -33,12 +38,60 @@ void test_hldlc_frame2(void) {
 
     check = HLDLC_frame(in, out, &size);
 
-    TEST_ASSERT_TRUE(check == SATR_EOT);
+    TEST_ASSERT_TRUE_MESSAGE(check == SATR_EOT, "Function didn't return SATR_EOT");
 
-    TEST_ASSERT_EQUAL_UINT8_ARRAY(res, out, res_size);
+    TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE(res, out, res_size, "Output wasn't the expected one");
 }
 
-void test_hldlc_deframe1(void) {
+void test_hldlc_frame_null_input(void) {
+    //Ignore debugging function
+    tst_debugging_IgnoreAndReturn(false);
+
+    uint8_t check;
+    uint16_t size = 10;
+    uint8_t *in = NULL;
+    uint8_t out[TEST_ARRAY*2];
+
+    check = HLDLC_frame(in, out, &size);
+
+    TEST_ASSERT_TRUE_MESSAGE(check == SATR_ERROR, "Function didn't return SATR_ERROR");
+}
+
+void test_hldlc_frame_null_output(void) {
+    //Ignore debugging function
+    tst_debugging_IgnoreAndReturn(false);
+
+    uint8_t check;
+    uint16_t size = 10;
+    uint8_t in[TEST_ARRAY] = { 0x12, 0x13, 0x14, 0x7E, 0x16, 0x17, 0x18, 0x7D, 0x22, 0x23};
+    uint8_t *out = NULL;
+
+    check = HLDLC_frame(in, out, &size);
+
+    TEST_ASSERT_TRUE_MESSAGE(check == SATR_ERROR, "Function didn't return SATR_ERROR");
+}
+
+void test_hldlc_frame_null_size(void) {
+    //Ignore debugging function
+    tst_debugging_IgnoreAndReturn(false);
+
+    uint8_t check;
+    uint16_t size;
+    uint8_t in[TEST_ARRAY] = { 0x12, 0x13, 0x14, 0x7E, 0x16, 0x17, 0x18, 0x7D, 0x22, 0x23};
+    uint8_t out[TEST_ARRAY*2];
+
+    check = HLDLC_frame(in, out, &size);
+
+    TEST_ASSERT_TRUE_MESSAGE(check == SATR_ERROR, "Function didn't return SATR_ERROR");
+}
+
+/*
+* ===========
+* | deframe |
+* ===========
+*/
+
+void test_hldlc_deframe_without_escape_octet(void) {
     uint8_t check;
     uint16_t size = 12;
     uint16_t res_size = 10, cnt_in = 0, cnt_out = 0;
@@ -49,11 +102,12 @@ void test_hldlc_deframe1(void) {
 
     check = HLDLC_deframe(in, out, &size);
 
-    TEST_ASSERT_TRUE(check == SATR_EOT);
-    TEST_ASSERT_TRUE(res_size == size);
-    TEST_ASSERT_EQUAL_UINT8_ARRAY(res, out, res_size);
+    TEST_ASSERT_TRUE_MESSAGE(check == SATR_EOT, "Function didn't return SATR_EOT");
+    TEST_ASSERT_TRUE_MESSAGE(res_size == size, "Output size was different than the expected");
+    TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE(res, out, res_size, "Output wasn't the expected one");
 }
-void test_hldlc_deframe2(void) {
+
+void test_hldlc_deframe_with_escape_octet(void) {
     uint8_t check;
     uint16_t size = 14;
     uint16_t res_size = 10, cnt_in = 0, cnt_out = 0;
@@ -64,17 +118,52 @@ void test_hldlc_deframe2(void) {
 
     check = HLDLC_deframe(in, out, &size);
 
-    TEST_ASSERT_TRUE(check == SATR_EOT);
-    TEST_ASSERT_TRUE(res_size == size);
+    TEST_ASSERT_TRUE_MESSAGE(check == SATR_EOT, "Function didn't return SATR_EOT");
+    TEST_ASSERT_TRUE_MESSAGE(res_size == size, "Output size was different than the expected");
+    TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE(res, out, res_size, "Output wasn't the expected one");
+}
 
-    TEST_ASSERT_EQUAL_UINT8_ARRAY(res, out, res_size);
+void test_hldlc_deframe_null_input(void) {
+    //Ignore debugging function
+    tst_debugging_IgnoreAndReturn(false);
+
+    uint8_t check;
+    uint16_t size = 10;
+    uint8_t *in = NULL;
+    uint8_t out[TEST_ARRAY*2];
+
+    check = HLDLC_frame(in, out, &size);
+
+    TEST_ASSERT_TRUE_MESSAGE(check == SATR_ERROR, "Function didn't return SATR_ERROR");
+}
+
+void test_hldlc_deframe_null_output(void) {
+    //Ignore debugging function
+    tst_debugging_IgnoreAndReturn(false);
+
+    uint8_t check;
+    uint16_t size = 10;
+    uint8_t in[TEST_ARRAY] = { 0x12, 0x13, 0x14, 0x7E, 0x16, 0x17, 0x18, 0x7D, 0x22, 0x23};
+    uint8_t *out = NULL;
+
+    check = HLDLC_frame(in, out, &size);
+
+    TEST_ASSERT_TRUE_MESSAGE(check == SATR_ERROR, "Function didn't return SATR_ERROR");
 }
 
 int main(void) {
     UNITY_BEGIN();
-    RUN_TEST(test_hldlc_frame1);
-    RUN_TEST(test_hldlc_frame2);
-    RUN_TEST(test_hldlc_deframe1);
-    RUN_TEST(test_hldlc_deframe2);
+
+    RUN_TEST(test_hldlc_frame_without_escape_octet);
+    RUN_TEST(test_hldlc_frame_with_escape_octet);
+    RUN_TEST(test_hldlc_frame_null_input);
+    RUN_TEST(test_hldlc_frame_null_output);
+    RUN_TEST(test_hldlc_frame_null_size);
+
+    RUN_TEST(test_hldlc_deframe_without_escape_octet);
+    RUN_TEST(test_hldlc_deframe_with_escape_octet);
+    RUN_TEST(test_hldlc_deframe_null_input);
+    RUN_TEST(test_hldlc_deframe_null_output);
+
     return UNITY_END();
 }
