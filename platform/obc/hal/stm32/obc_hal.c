c4971800f3ff6653972e90913e00cc9ba789de1f
nchronas <nchronas@gmail.com>
2016-04-22 00:14:15 +0300

    added sram backup power on

--- platform/obc/hal/stm32/obc_hal.c
+++ platform/obc/hal/stm32/obc_hal.c
@@ -205,8 +205,8 @@ void HAL_sys_getDate(uint8_t *mon, uint8_t *date, uint8_t *year) {
 }
 
 void HAL_obc_enableBkUpAccess() {
-  
   HAL_PWR_EnableBkUpAccess();
+  HAL_PWREx_EnableBkUpReg();
   __HAL_RCC_BKPSRAM_CLK_ENABLE();
   
 }
